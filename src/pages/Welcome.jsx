33a78bcc3e8bf6289adb87fff6ba0d470aa391a6
Thiago Volpi Rabethge~ <thiago.rabethge@gmail.com>
2024-07-11 19:38:50 -0300

    feature: add nav, add logout button

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import useUserSessionStore from '../data/userSession'
+import Nav from '../components/Nav'
 
 const Welcome = () => {
   const userSession = useUserSessionStore(state => state.userSession)
@@ -26,52 +27,56 @@ const Welcome = () => {
   }`
 
   return (
-    <div className="container">
-      <div className="text-center mt-5">
-        <h2>Getting started</h2>
+    <>
+      <div className="container">
+        <div className="text-center mt-4">
+          <h2>Getting started</h2>
+        </div>
+
+        <p>
+          Welcome dear {userSession.username}, we are happy to see you here, follow the steps above
+        </p>
+
+        <ol>
+          <li>
+            Add a manifest json in your project root:
+            <dl>
+              <dt>
+                <code>
+                  {code}
+                </code>
+              </dt>
+            </dl>
+          </li>
+
+          <li>
+            Add manifest link in your index.html:
+            <dl>
+              <dt>
+                <code>
+                  {`<link rel="manifest" href="/manifest.json" />`}
+                </code>
+              </dt>
+            </dl>
+          </li>
+
+          <li>
+            Build and deploy!
+            <dl>
+              <dt>
+                <p
+                  style={{ "fontWeight": "normal" }}
+                >
+                  Now, you only need build your application and deploy it
+                </p>
+              </dt>
+            </dl>
+          </li>
+        </ol>
       </div>
 
-      <p>
-        Welcome dear {userSession.username}, we are happy to see you here, follow the steps above
-      </p>
-
-      <ol>
-        <li>
-          Add a manifest json in your project root:
-          <dl>
-            <dt>
-              <code>
-                {code}
-              </code>
-            </dt>
-          </dl>
-        </li>
-
-        <li>
-          Add manifest link in your index.html:
-          <dl>
-            <dt>
-              <code>
-                {`<link rel="manifest" href="/manifest.json" />`}
-              </code>
-            </dt>
-          </dl>
-        </li>
-
-        <li>
-          Build and deploy!
-          <dl>
-            <dt>
-              <p
-                style={{ "fontWeight": "normal" }}
-              >
-                Now, you only need build your application and deploy it
-              </p>
-            </dt>
-          </dl>
-        </li>
-      </ol>
-    </div>
+      <Nav />
+    </>
   )
 }
 
