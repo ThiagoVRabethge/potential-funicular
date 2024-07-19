01cc0ec0b16f281f7724f51abdb0e8bc6294764c
Thiago Volpi Rabethge~ <thiago.rabethge@gmail.com>
2024-07-19 00:22:56 -0300

    feature: add all apps page

---
+++ src/pages/Store.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react"
+import Nav from "../components/Nav"
+import api from "../services/api"
+
+const Store = () => {
+  const [allAppsList, setAllAppsList] = useState()
+
+  useEffect(() => {
+    getAllApps()
+  }, [])
+
+  const getAllApps = async () => {
+    await api
+      .get("/apps")
+      .then((response) => {
+        setAllAppsList(response.data)
+      })
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="mt-4 mb-4">
+          <h3>All apps available</h3>
+        </div>
+
+        {allAppsList && allAppsList.map((app) => (
+          <div key={app.id} className="card">
+            <div className="card-body">
+              <h6>{app.name}</h6>
+
+              <p>{app.description}</p>
+
+              <a href={app.link} target="_blank">
+                {app.link}
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <Nav />
+    </>
+  )
+}
+
+export default Store
